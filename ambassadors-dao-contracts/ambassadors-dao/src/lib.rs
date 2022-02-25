2753ff0563c2b28da242fad319fec2da007d5539
gitshreevatsa <vastas2002@gmail.com>
2022-02-25 12:52:16 +0530

    None

---
+++ ambassadors-dao-contracts/ambassadors-dao/src/lib.rs
@@ -0,0 +1,37 @@
+//! Contains the Contract struct and its implementation
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LookupMap;
+use near_sdk::{near_bindgen, PanicOnDefault};
+
+use payout::Payout;
+
+mod payout;
+pub mod views;
+mod vote;
+
+/// The main contract governing Ambassadors DAO
+#[near_bindgen]
+#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
+pub struct Contract {
+    proposals: LookupMap<u64, Payout>,
+    bounties: LookupMap<u64, Payout>,
+}
+
+#[near_bindgen]
+impl Contract {
+    #[init]
+    pub fn new() -> Self {
+        Self {
+            bounties: LookupMap::<u64, Payout>::new(b"b".to_vec()),
+            proposals: LookupMap::<u64, Payout>::new(b"p".to_vec()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn generates_contract() {
+        unimplemented!()
+    }
+}
